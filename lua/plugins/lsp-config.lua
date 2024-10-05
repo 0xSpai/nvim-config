8f67db667fbea30e1485f3d3255f0004e3f1f2bc
0xSpai <spairoblox@gmail.com>
2024-10-05 15:04:25 +0200

    Initial commit

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "lopi-py/luau-lsp.nvim",
+    config = function()
+      require("luau-lsp").setup {
+        platform = {
+          type = "roblox",
+        },
+        types = {
+          roblox_security_level = "PluginSecurity",
+        },
+      }
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "luau_lsp" }
+      })
+
+      require("mason-lspconfig").setup_handlers({
+        function(server_name)
+          require("lspconfig")[server_name].setup {}
+        end,
+        ["luau_lsp"] = function()
+          require("luau-lsp").setup {
+            platform = {
+              type = "roblox",
+            },
+            types = {
+              roblox_security_level = "PluginSecurity",
+            },
+          }
+        end,
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+  }
+}
+
