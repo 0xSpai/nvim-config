5a40c547c9b5285acd368b5f1de60ff812884368
0xSpai <spairoblox@gmail.com>
2024-10-07 00:14:25 +0200

    added lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,58 +1,61 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "lopi-py/luau-lsp.nvim",
-    config = function()
-      require("luau-lsp").setup {
-        platform = {
-          type = "roblox",
-        },
-        types = {
-          roblox_security_level = "PluginSecurity",
-        },
-      }
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "luau_lsp" }
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"lopi-py/luau-lsp.nvim",
+		config = function()
+			require("luau-lsp").setup({
+				platform = {
+					type = "roblox",
+				},
+				types = {
+					roblox_security_level = "PluginSecurity",
+				},
+			})
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "luau_lsp" },
+			})
+
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({})
+				end,
+				["luau_lsp"] = function()
+					require("luau-lsp").setup({
+						platform = {
+							type = "roblox",
+						},
+						types = {
+							roblox_security_level = "PluginSecurity",
+						},
+					})
+				end,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+        capabilities = capabilities
       })
 
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup {}
-        end,
-        ["luau_lsp"] = function()
-          require("luau-lsp").setup {
-            platform = {
-              type = "roblox",
-            },
-            types = {
-              roblox_security_level = "PluginSecurity",
-            },
-          }
-        end,
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end
-  }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
-
